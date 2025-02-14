b31f3d7304cf4497e4aef33d58316b58d2e48344
LeoGNavaC <leonava988@gmail.com>
2025-02-14 17:08:54 -0600

    El primer commit de este proyecto

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bosques de Santa Fe</title>
+    <link rel="icon" type="image/*" href="Imagenes/BSFicon.png">
+  </head>
+
+  <body>
+    <h1>Bosques de Santa Fe</h1>
+  </body>
+</html>
