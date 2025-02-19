3a281dfb23c3f9814b05da8104b764677429c3a2
LeoGNavaC <leonava988@gmail.com>
2025-02-19 11:26:09 -0600

    Eliminamos el estilo pero por alguna razon no nos deja cambiar a otro estilo

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
   <head>
     <meta charset="UTF-8">
     <title>Bosques de Santa Fe</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/*" href="Imagenes/BSFicon.png">
+    <link rel="stylesheet" href="estilos/estilos.css">
   </head>
 
   <body>
